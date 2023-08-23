8367b654b41a9f6f3a0cccfb10dffad40feacd63
Joshua Zhou <josh@Joshuas-MacBook-Pro.local>
2023-08-23 12:18:24 -0600

    finish

--- app/src/Components/Account Section/Account.jsx
+++ app/src/Components/Account Section/Account.jsx
@@ -1,9 +1,52 @@
 import React from 'react';
 import './account.css'
 
+import {BiChevronRight, BiEdit} from 'react-icons/bi';
+import {BsThreeDots} from 'react-icons/bs';
+
+import user from '../../Assets/image.jpeg'
+
+
 const Account = () => {
     return (
-        <div>Account</div>
+        <div className='account'>
+            <div className="icons flex">
+                <BiChevronRight className='icon'/>
+                <BsThreeDots className='icon'/>
+            </div>
+
+            <div className="accountDetails">
+
+                <div className="imageDiv">
+                    <img src={user} alt="account user image" />
+                    <span className='name'>Name</span>
+                    <span className='position'>Student</span>
+                    <button className="btn flex">
+                        <BiEdit className='icon'/>
+                        Edit Profile
+                    </button>
+                </div>
+
+                <div className="hoursDiv">
+                    <span className="title">Working Hours</span>
+                    <div className="hoursCard flex">
+
+                        <div className="right bg">
+                            <span>Work Starts</span>
+                            <h6>Time</h6>
+                        </div>
+
+                        <div className="left noBg">
+                            <span>Work Ends</span>
+                            <h6>Time</h6>
+                        </div>
+
+                    </div>
+                </div>
+                <h1 className='city'>City</h1>
+                <span className="region">Country, Timezone</span>
+            </div>
+        </div>
     )
 }
 
--- app/src/Components/Body Section/BottomSection/Bottom.jsx
+++ app/src/Components/Body Section/BottomSection/Bottom.jsx
@@ -1,10 +1,104 @@
-import React from 'react'
+import { React, useState } from 'react'
 import './bottom.css'
 
+import Chart from "react-apexcharts";
+import img from '../../../Assets/laptop.jpg'
+import img2 from '../../../Assets/dimensions.png'
+import img3 from '../../../Assets/animation.png'
+
 const Bottom = () => {
+
+  const [state, setState] = useState({
+    options: {
+      colors: ['#01565B', '#5ABA8A'],
+      chart: {
+        id: 'basic-bar',
+        toolbar: {
+          show: true,
+          tools: {
+            selection: false,
+            zoom: false,
+            zoomin: false,
+            zoomout: false,
+            pan: false,
+            reset: true | '<img src="/static/icns/reset.png" width="10">',
+            customIcons: []
+          }
+
+        },
+      },
+
+      xaxis: {
+        categories: ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat']
+      }
+    },
+
+    series: [
+      {
+        name: 'Updates',
+        data: [25, 33, 13, 89, 34, 20, 30]
+      },
+      {
+        name: 'New Tasks',
+        data: [70, 47, 72, 45, 12, 59, 30]
+      }
+    ]
+  })
+
   return (
-    <div>
-      Bottom
+    <div className='bottom flex'>
+
+      <div className="graphDiv">
+        <div className="title">All Activities (Graphical View)</div>
+
+        <Chart 
+        options={state.options}
+        series={state.series}
+        type='area'
+        width='500'
+        />
+      </div>
+
+      <div className="scheduleDiv">
+        <span className="title">Upcoming Schedule</span>
+
+        <div className="singleTask flex">
+          <div className="imgDiv flex">
+            <img src={img} alt="Task Image" />
+          </div>
+          <div className="taskInfo">
+            <span className="task">Task</span>
+            <span className="status">Working On</span>
+          </div>
+          <span className="time">Time</span>
+        </div>
+
+        <div className="singleTask flex">
+          <div className="imgDiv flex">
+            <img src={img2} alt="Task Image" />
+          </div>
+          <div className="taskInfo">
+            <span className="task">Task</span>
+            <span className="status">Working On</span>
+          </div>
+          <span className="time">Time</span>
+        </div>
+
+        <div className="singleTask flex">
+          <div className="imgDiv flex">
+            <img src={img3} alt="Task Image" />
+          </div>
+          <div className="taskInfo">
+            <span className="task">Task</span>
+            <span className="status">Working On</span>
+          </div>
+          <span className="time">Time</span>
+        </div>
+
+        <div className="btn">
+          See All Activities
+        </div>
+      </div>
     </div>
   )
 }
